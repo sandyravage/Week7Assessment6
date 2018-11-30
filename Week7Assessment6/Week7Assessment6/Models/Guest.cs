1cdbf76fae06a3a9dff09c8013dce0f230b0aff8
Taylor Everts <tayloreverts@quickenloans.com>
2018-11-30 16:13:35 -0500

    sub1

---
+++ Week7Assessment6/Week7Assessment6/Models/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week7Assessment6.Models
+{
+    public class Guest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Attending { get; set; }
+        public string PartyDate { get; set; }
+        public string PlusOne { get; set; }
+        public string GuestName { get; set; }
+
+        public string GetPartyDate()
+        {
+            string date = "";
+            if (PartyDate == "1")
+            {
+                date = "December 15th, 2018";
+            }
+            else
+            {
+                date = "December 18th, 2018";
+            }
+            return date;
+        }
+    }
+}
